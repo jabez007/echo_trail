3e389b71f4b1b0ebe97ffaf4114744a917e3f0a2
Jimmy <jabez007@users.noreply.github.com>
2025-04-07 11:27:42 -0500

    Update setup.py to match pyproject

--- setup.py
+++ setup.py
@@ -3,7 +3,16 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='mccann_hub-echo_trail',
     version='0.1.0',
-    packages=find_namespace_packages(include=['mccann_hub.*']),
-    install_requires=[],
+    packages=find_namespace_packages(where='src', include=['mccann_hub.*']),
+    install_requires=[
+        "python-json-logger>=3.0.0"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires=">=3.9",
 )
 
